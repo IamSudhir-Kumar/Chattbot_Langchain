43e41fbf688d0c59f6ad941ffcc79d139313f4ae
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-11-07 15:56:55 +0530

    console is not working the problem is in running the program

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import { RecursiveCharacterTextSplitter } from './node_modules/langchain/text_splitter'
 
+try {
+  const result = await fetch('myinfo.txt')
+  const text = await result.text()
+  
+  const splitter = new RecursiveCharacterTextSplitter()
+  
+  const output = await splitter.createDocuments([text])
+  console.log(output)
+} catch (err) {
+  console.log(err)
+}
